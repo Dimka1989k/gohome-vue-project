adc2f4bee1945a85c407750a9137bad360c18435
Dimon1989k <106411942+Dimon1989k@users.noreply.github.com>
2023-08-29 01:42:18 +0300

    add cards apartments

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+
+<ApartmentsItem
+:descr="apartment.descr"
+  :price="apartment.price"
+   :rating="apartment.rating"
+   imgSrc="./img/apartment/apartment-1.jpg"/>
+</template>
+
+<script>
+import ApartmentsItem from './components/apartment/ApartmentsItem.vue';
+
+
+
+
+
+export default {
+  name: 'App',
+  components: { 
+    ApartmentsItem,
+  },
+  data() {
+    return {
+      apartment: {            
+      id: '5f689a2c09b16000e9a2f47b',
+        title: 'Delectus et iste.',
+        
+        descr:
+          'Debitis similique unde et eligendi sapiente in iure blanditiis distinctio. Cum nihil temporibus qui quam aut. Nesciunt qui eveniet eum quam voluptate numquam.',
+        rating: 3,
+        price: 1667,
+        location: {
+          city: 'Dnipro',
+        },
+        owner: {
+          name: 'Bobbi',
+          phone: '582-740-2926',
+          email: 'Pete_Carter53@hotmail.com',
+        },      
+    },
+        }
+      }
+    }
+ 
+  
+
+
+</script>
+
+<style>
+#app {
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
